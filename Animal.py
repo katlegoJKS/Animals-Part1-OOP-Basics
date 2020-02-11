f1196f205479ab31ed738e318c99fd6854fb44d6
katlegoJKS <katlego.malatjie@umuzi.org>
2020-02-11 15:11:18 +0200

    first commit

---
+++ Animal.py
@@ -0,0 +1,23 @@
+class Animal:
+
+    def __init__(self,eats,sounds):
+        self.eats = eats
+        self.sounds = sounds
+
+
+class Dog(Animal):
+    
+    def __init__sound(self):
+        Animal.__init__(self,eats,sounds)
+
+class Cat(Animal):
+    
+    def __init__sound(self):
+        Animal.__init__(self,eats,sounds)
+
+mpya = Dog('dogmore','barks')
+katse = Animal('catmore','meows')
+
+
+mpya.Dog()
+print("Dog")
