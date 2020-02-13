3e0d604af74cd3654ca6ef177ebc200b1afc9a34
katlegoJKS <katlego.malatjie@umuzi.org>
2020-02-13 15:49:07 +0200

    Home

--- home.py
+++ home.py
@@ -4,5 +4,23 @@ from cat import Cat
 
 class Home:
     
-    def __init__(self,name,sound):
-        super().__init__(name,sound)
+    def __init__(self,pets = []):
+        self.pets = pets
+
+    def adopt_pet(self,pet):
+        for each_pet in self.pets:
+            if each_pet == pet:
+                raise Exception("You cannot adopt the same pet twice")
+            self.pets.append(pet)
+
+    def make_all_sounds():
+        return dog1.food()
+
+home = Home()
+dog1 = Dog("Rax","barks")
+cat1 = Cat("Stormy","meows")
+home.adopt_pet(dog1)
+home.adopt_pet(cat1)
+
+print("Current pets: ")
+print(home.pets)
